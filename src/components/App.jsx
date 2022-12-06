73c412aee89ace025c76beec287ef4093f878f8b
antony-chi <antony.cp9@gmail.com>
2022-12-06 16:37:46 -0600

    se agrego boostrap al proyecto

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,10 +8,10 @@ function App() {
     <div className="App">
       <div>
         <a href="https://vitejs.dev" target="_blank">
-          <img src="/public/img/vite.svg" className="logo" alt="Vite logo" />
+          <img src="./img/vite.svg" className="logo" alt="Vite logo" />
         </a>
         <a href="https://reactjs.org" target="_blank">
-          <img src="./public/img/react.svg" className="logo react" alt="React logo" />
+          <img src="./img/react.svg" className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,13 @@
 import { StrictMode} from 'react'
 import {createRoot} from 'react-dom/client'
+/* components*/
 import App from './components/App'
+/* styles*/
+import 'bootstrap/dist/css/bootstrap.css'
 import './styles/index.css'
 
-createRoot(document.getElementById('root')).render(
+const root =  createRoot(document.getElementById('root'))
+root.render(
   <StrictMode>
     <App />
   </StrictMode>
